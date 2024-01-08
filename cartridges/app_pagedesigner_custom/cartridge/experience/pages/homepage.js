f609ed99a462f369d6b8587c43b4f789189737bb
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-01-08 13:05:42 +0400

    page homepage - editing script file and  template

--- cartridges/app_pagedesigner_custom/cartridge/experience/pages/homepage.js
+++ cartridges/app_pagedesigner_custom/cartridge/experience/pages/homepage.js
@@ -1,14 +1,52 @@
+// 'use strict';
+// var Template = require('dw/util/Template');
+// var HashMap = require('dw/util/HashMap');
+// /**
+//  * Render logic for the page.
+//  */
+// module.exports.render = function (context) {
+//     var model = new HashMap();
+//    // add parameters to model as required for rendering based on the given context (dw.experience.PageScriptContext):
+//    // * context.page (dw.experience.Page)
+//    // * context.renderParameters (String)
+//    // * context.content (dw.util.Map)
+//     return new Template('experience/pages/homepage').render(model).text;
+// };
 'use strict';
+/* global response */
+
 var Template = require('dw/util/Template');
 var HashMap = require('dw/util/HashMap');
+var PageRenderHelper = require('*/cartridge/experience/utilities/PageRenderHelper.js');
+
 /**
- * Render logic for the page.
+ * Render logic for the storepage.
+ *
+ * @param {dw.experience.PageScriptContext} context The page script context object.
+ * @param {dw.util.Map} [modelIn] Additional model values created by another cartridge. This will not be passed in by Commcerce Cloud Plattform.
+ *
+ * @returns {string} The markup to be displayed
  */
-module.exports.render = function (context) {
-    var model = new HashMap();
-   // add parameters to model as required for rendering based on the given context (dw.experience.PageScriptContext):
-   // * context.page (dw.experience.Page)
-   // * context.renderParameters (String)
-   // * context.content (dw.util.Map)
+module.exports.render = function (context, modelIn) {
+    var model = modelIn || new HashMap();
+
+    var page = context.page;
+    model.page = page;
+    model.content = context.content;
+
+    // automatically register configured regions
+    model.regions = PageRenderHelper.getRegionModelRegistry(page);
+
+    if (PageRenderHelper.isInEditMode()) {
+        var HookManager = require('dw/system/HookMgr');
+        HookManager.callHook('app.experience.editmode', 'editmode');
+        model.resetEditPDMode = true;
+    }
+
+    model.CurrentPageMetaData = PageRenderHelper.getPageMetaData(page);
+
+	// no pagecache setting here, this is dynamically determined by the components used within the page
+
+    // render the page
     return new Template('experience/pages/homepage').render(model).text;
 };
