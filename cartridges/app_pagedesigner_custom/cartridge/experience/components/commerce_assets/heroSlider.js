55c05951efab6c491de107df6ce8608d3ecc6325
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-01-08 16:54:44 +0400

    created layout component 3x1, and working on hero slider

--- cartridges/app_pagedesigner_custom/cartridge/experience/components/commerce_assets/heroSlider.js
+++ cartridges/app_pagedesigner_custom/cartridge/experience/components/commerce_assets/heroSlider.js
@@ -1,14 +1,24 @@
 'use strict';
+
 var Template = require('dw/util/Template');
 var HashMap = require('dw/util/HashMap');
 /**
  * Render logic for the component.
  */
 module.exports.render = function (context) {
-   var model = new HashMap();
-   // add parameters to model as required for rendering based on the given context (dw.experience.ComponentScriptContext):
-   // * context.component (dw.experience.Component)
-   // * context.content (dw.util.Map)
-   // * model.bannerMessage = context.content.alt;
- return new Template('experience/components/commerce_assets/heroSlider').render(model).text;
+    var model = new HashMap();
+    model.title = context.content.title;
+    model.image = context.content.image;
+    model.alt = context.content.alt;
+    model.width = context.content.width;
+    model.height = context.content.height;
+    model.shortDescription = context.content.shortDescription;
+    model.ctaTitle = context.content.ctaTitle;
+
+    // instruct 24 hours relative pagecache
+    var expires = new Date();
+    expires.setDate(expires.getDate() + 1); // this handles overflow automatically
+    response.setExpires(expires);
+
+    return new Template('experience/components/commerce_assets/heroSlider').render(model).text;
 };
--- cartridges/app_pagedesigner_custom/cartridge/experience/pages/homepage.js
+++ cartridges/app_pagedesigner_custom/cartridge/experience/pages/homepage.js
@@ -1,17 +1,3 @@
-// 'use strict';
-// var Template = require('dw/util/Template');
-// var HashMap = require('dw/util/HashMap');
-// /**
-//  * Render logic for the page.
-//  */
-// module.exports.render = function (context) {
-//     var model = new HashMap();
-//    // add parameters to model as required for rendering based on the given context (dw.experience.PageScriptContext):
-//    // * context.page (dw.experience.Page)
-//    // * context.renderParameters (String)
-//    // * context.content (dw.util.Map)
-//     return new Template('experience/pages/homepage').render(model).text;
-// };
 'use strict';
 /* global response */
 
