c9af712a90fdfbd8fdf8aef0eda7117f808219f6
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-01-09 18:28:28 +0400

    custom category component

---
+++ cartridges/app_pagedesigner_custom/cartridge/experience/utilities/ImageTransformation.js
@@ -0,0 +1,45 @@
+var ImageTransformation = {};
+var BREAKPOINTS = require('*/cartridge/experience/breakpoints.json');
+var Image = require('dw/experience/image/Image');
+var MediaFile = require('dw/content/MediaFile');
+var base = module.superModule;
+
+
+/**
+ * Return an object containing the scaled image for mobile, table and desktop.  Other included image details
+ * are: alt text, focalPoint x, focalPoint y.
+ *
+ * @param {Image} image the image for which to be scaled.
+ * @param {Object} The object containing the scaled image
+ *
+ * @return {string} The Absolute url
+ */
+base.getScaledImage = function (image) {
+    var imageFile = image instanceof MediaFile ? image : image.file;
+    if (image instanceof MediaFile) {
+        return {
+            src: {
+                mobile: base.url(imageFile, { device: 'mobile' }),
+                tablet: base.url(imageFile, { device: 'tablet' }),
+                desktop: base.url(imageFile, { device: 'desktop' })
+            },
+            alt: imageFile.getAlt(),
+            focalPointX: '100%',
+            focalPointY: '100%'
+        };
+    } else {
+        return {
+            src: {
+                mobile: base.url(imageFile, { device: 'mobile' }),
+                tablet: base.url(imageFile, { device: 'tablet' }),
+                desktop: base.url(imageFile, { device: 'desktop' })
+            },
+            alt: imageFile.getAlt(),
+            focalPointX: (image.focalPoint.x * 100) + '%',
+            focalPointY: (image.focalPoint.y * 100) + '%'
+        };
+    }
+    
+};
+
+module.exports = base;
